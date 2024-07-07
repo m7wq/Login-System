159479757ee21c4273cd5f809a5ecb8508bd9204
M7moud_ <seekpotato47@gmail.com>
2024-07-08 01:36:09 +0300

    Add files via upload

---
+++ src/Contents/CommanContents.java
@@ -0,0 +1,1 @@
+package Contents;public class CommanContents {    public final static String APP_NAME = "LOGIN SYSTEM";    public final static int[] FRAME_SIZE = {400,400};    public final static int FIELD_SIZE = 15;}
