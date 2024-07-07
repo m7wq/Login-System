122b4b07914a567f7a7cdc8a52434ffa0841675d
M7moud_ <seekpotato47@gmail.com>
2024-07-08 01:59:05 +0300

    Add files via upload

---
+++ src/Contents/CommonContents.java
@@ -0,0 +1,8 @@
+package Contents;
+
+public class CommonContents {
+
+    public final static String APP_NAME = "LOGIN SYSTEM";
+    public final static int[] FRAME_SIZE = {400,400};
+    public final static int FIELD_SIZE = 15;
+}
---
+++ src/LoginApp.java
@@ -0,0 +1,10 @@
+import guis.RegisterGui;
+
+public class LoginApp {
+
+
+    public static void main(String[] args){
+        RegisterGui.registerGui.setVisible(true);
+    }
+
+}
