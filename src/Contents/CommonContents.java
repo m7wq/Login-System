e16a52bb6a9fc91253ad449f022c0a3f5a102f50
M7moud_ <seekpotato47@gmail.com>
2024-07-08 01:58:43 +0300

    Delete src directory

--- src/Contents/CommonContents.java
+++
@@ -1,8 +0,0 @@
-package Contents;
-
-public class CommonContents {
-
-    public final static String APP_NAME = "LOGIN SYSTEM";
-    public final static int[] FRAME_SIZE = {400,400};
-    public final static int FIELD_SIZE = 15;
-}
