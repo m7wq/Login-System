8cda02defc43643fed1c0bd85091d0fe54304f04
M7moud_ <seekpotato47@gmail.com>
2024-07-08 01:55:51 +0300

    Add files via upload

---
+++ src/Account/Registrable.java
@@ -0,0 +1,15 @@
+package Account;
+
+public interface Registrable {
+
+    String getGmail();
+    String getUser();
+    String getPassword();
+
+    void setGmail(String gmail);
+    void setUser(String user);
+    void setPassword(String password);
+    
+
+
+}
